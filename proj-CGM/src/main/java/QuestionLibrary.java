16e28f69c3e8fd81499c5174782cb94bdd0096c3
Napasmix <90303008+Napasmix@users.noreply.github.com>
2024-04-21 23:25:19 +0200

    Add files via upload

--- proj-CGM/src/main/java/QuestionLibrary.java
+++ proj-CGM/src/main/java/QuestionLibrary.java
@@ -2,20 +2,29 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class QuestionLibrary {
+    // A map to store questions as keys and arrays of answers as values
     private Map<String, String[]> questionsAndAnswers;
 
+
+    // Method to add a question and its answers to the library
     public QuestionLibrary() {
         this.questionsAndAnswers = new HashMap<>();
     }
 
+
+     // Method to retrieve answers for a given question
     public void addQuestionAndAnswers(String question, String[] answers) {
         questionsAndAnswers.put(question, answers);
     }
 
+
+     // Method to retrieve answers for a given question
     public String[] getAnswers(String question) {
         return questionsAndAnswers.get(question);
     }
 
+
+      // Method to retrieve all questions and their answers
     public Map<String, String[]> getAllQuestionsAndAnswers() {
         return questionsAndAnswers;
     }
